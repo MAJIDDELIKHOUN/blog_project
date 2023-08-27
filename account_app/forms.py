40041f7bf4a53387d8c8cfa9c2b06f8cb3bd083d
majid <majiddelikhoun@gmail.com>
2023-08-27 16:23:14 +0430

    update1

---
+++ account_app/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.forms import ValidationError
+from django.contrib.auth.forms import UserCreationForm
+from django.forms.widgets import PasswordInput
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=50,widget=forms.TextInput({'class': 'form-control', 'placeholder': "Username"}))
+    password = forms.CharField(widget=forms.PasswordInput({'class': "form-control", 'placeholder': "Password"}))
+
+    def clean_password(self):
+        user = authenticate(username=self.cleaned_data.get('username'), password=self.cleaned_data.get('password'))
+        if user is not None:
+            return self.cleaned_data.get('password')
+        raise ValidationError('username or password wrong', code='invalid info')
+
+
+# class RegisterForm(UserCreationForm):
+#     username = forms.CharField(max_length=50, widget=forms.TextInput({'class': 'form-control', 'placeholder': "Username"}))
+#     password1 = forms.CharField(widget=forms.PasswordInput({'class': "form-control", 'placeholder': "Password1"}))
+#     password2 = forms.CharField(widget=forms.PasswordInput({'class': "form-control", 'placeholder': "Password2"}))
+#     email=forms.EmailField(widget=forms.TextInput({'class': 'form-control', 'placeholder': "email"}))
+# def clean_password(self):
+#     user = authenticate(username= self.cleaned_data.get('username'),password=self.cleaned_data.get('password'))
+#     if user is not None:
+#         return self.cleaned_data.get('password')
+#     raise ValidationError('username or password wrong',code='invalid info')
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2',)
+        # self.fields['username'].widget = widgets.TextInput(
+        #     attrs={
+        #         'placeholder': 'Username',
+        #         'class': 'form-control',
+        #         'style': 'margin-bottom: 10px'})
+
+        # widgets = {'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': "Username"}),
+        #            'password1': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': "Username"}),
+        #            'password2': forms.PasswordInput(),
+        #            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': "email"})}
